3d8100ff9a2a860d5086e6536688679eeadb54ad
PROD\wdegruijl <wouter.degruijl@alten.nl>
2022-11-25 14:09:19 +0100

    ready for deployment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faker } from "@faker-js/faker";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'typing-challenge';
+  randomText: string = faker.lorem.sentence();
+  enteredText: string = '';
+
+  currentDateTime = new Date;
+  startTime: number = this.currentDateTime.getTime();
+  completionTime: number = 0;
+
+  detectTyping(event: any) {
+    this.enteredText = event.target.value;
+    if (this.enteredText === this.randomText) {
+      const finishDateTime = new Date;
+      this.completionTime = ((finishDateTime.getTime() - this.startTime)/1000)/this.randomText.length;
+    }
+  }
+
+  compare(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+  }
+
+  reloadGame() {
+    window.location.reload();
+  }
 }
